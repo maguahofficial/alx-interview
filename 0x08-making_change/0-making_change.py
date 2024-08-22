5fe5b3b39eba4118b9bb81d7d71b2a3dfbf59fd8
maguah <maguahofficial@gmail.com>
2024-08-23 01:20:08 +0300

    0x08-making_change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" 0. The Change comes from within """
+
+
+def makeChange(coins, total):
+    """ (function) Given a pile of coins of different values,
+    determine the fewest number
+    of coins needed to meet a given amount total.
+    """
+    if total < 1:
+        return 0
+    coins.sort()
+    coins.reverse()
+    fewest = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        buff = total // coin
+        fewest += buff
+        total -= (buff * coin)
+    if total != 0:
+        return -1
+    return fewest
